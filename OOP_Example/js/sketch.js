75f9e0d32890ffe8da5381eeccfaa9a0d16f6eb1
Madebyleon <exilente@gmail.com>
2025-02-10 13:50:26 -0500

    updated

--- OOP_Example/js/sketch.js
+++ OOP_Example/js/sketch.js
@@ -1,6 +1,8 @@
 
 
 let balls = []; // This is an empty array
+let img;
+
 
 function setup() {
 	
@@ -8,26 +10,56 @@ function setup() {
 	
 
 noStroke();
+colorMode(HSB);
 // var b = new Bubbles(); 
 // // This is me making a new instance or cookie
 // // from my cookie cutter.
 
+
+textAlign(CENTER);
+textStyle(BOLD);
+noCursor();
+
+  let link = createA("https://nl06cvmfdn2.typeform.com/to/SC1Hpi1Z", "Sign Up Here!", "_blank");
+
+  link.position(width/2, 700);
+  link.style("font-size", "50px");
+  link.style("color", "black");
 }
 
 function draw() {
 	
-// background(200);
+background(200, 25, 100);
 
 
 	for (let i = 0; i<balls.length; i++) {
+		fill(i,255,255);
 		balls[i].display();
 		balls[i].bounce();
-	print(balls[i]);
+		// print(balls[i]);
 	}
 
+// image(img, width/2, height/2);
 
-	}
+push();
 
+translate(width/2, height/2);
+textFont('Helvetica');
+textSize(200);
+fill(150);
+text('ICCIT', 0, -50);
+
+textSize(100);
+fill(150);
+text('End of Year', 0, 65);
+
+textSize(200);
+fill(150);
+text('Show!!', 0, 250);
+
+
+pop();
+}
 
 
 
@@ -40,7 +72,7 @@ b.y = mouseY;
 b.r = random (255);
 b.g = random (255);
 b.b = random (255);
-b.size = random(40,100);
+b.size = random(20,50);
 b.speed = random (1,10);
 
 balls.push(b);
@@ -68,7 +100,7 @@ class Bubbles { // Cookie cutter
 	}
 
 	display () {
-		fill(this.r, this.g, this.b);
+	//	fill(this.r, this.g, this.b);
 		ellipse(this.x, this.y, this.size, this.size);
 		this.y = this.y + this.speed;
 		// This will make it go down. 
